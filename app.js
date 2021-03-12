e230199e552292b407520bad7c66789970977e3c
Tahmid Zawad <tahmid@lingellearning.com>
2021-03-12 15:55:43 -0500

    Add some more code for fun and altering game behaviour

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let choices = ['Rock', 'Papers', 'Scissors']; // available choices
 let choicesindex = [0, 1, 2];                 // numerification of available choices
-let roundsrange = [1, 2, 3, 4, 5];            // list of accepted number of rounds
 let yourwinsets = ['02', '10', '21'];         // PLAYER vs. COMP combos where the player wins
 let yourtotalwins = 0;                        // global initial value for player wins
 let yourtotallosses = 0;                      // global initial value for player losses
+let maxrounds = 10000;                        // list of accepted number of rounds
+// let yourwincountpct = 0.00;                // percent of wins out of total wins+losses
 
 function computerPlay() {
     let indexrange = 10;
@@ -15,17 +16,18 @@ function computerPlay() {
 function playRound(yourindex, compindex) {
     let winner;
     if (yourindex == compindex) {
-        return "None!";
+        winner = "None!";
     } else {
         yourplayset = yourindex.toString() + compindex.toString();
         if (yourwinsets.includes(yourplayset)) {
             yourtotalwins++;
-            return "You!" 
+            winner = "You!" 
         } else {
             yourtotallosses++;
-            return  "Computer!";
+            winner =  "Computer!";
         }
     }
+    return winner;
 }
 
 function game(roundname) {
@@ -37,6 +39,7 @@ function game(roundname) {
             console.log("Invalid submission! Asking again!");
         }
     }
+    // yourindex = computerPlay(); // treating user as another 'computer' 
     let compindex = computerPlay();
     console.log('You chose: ' + choices[yourindex]);
     console.log('Computer chose: ' + choices[compindex]);
@@ -46,12 +49,20 @@ function game(roundname) {
 /* main */
 console.log('--- BEGINNING MATCH ---');
 let numrounds = parseInt(prompt("How many rounds?"));
-if (!(Number.isInteger(numrounds) && roundsrange.includes(numrounds))) {
-    console.log(`We do max ${Math.max(...roundsrange)} rounds. Fuck off!`);
+if (!(Number.isInteger(numrounds) && numrounds <= maxrounds)) {
+    console.log(`We do min 1 to max ${maxrounds} rounds. Fuck off!`);
 } else {
     for (round = 1; round <= numrounds; round++) {
         game(round);
     }
+    /* See how long it takes for wins to dominate *//*
+    let round = 1;
+    while (yourwincountpct < 50.00) {
+        game(round);
+        round++;
+        yourwincountpct = 100*(yourtotalwins / (yourtotalwins + yourtotallosses));
+    }
+    */
     console.log('[Match Summary]');
     console.log("Your total wins: " + yourtotalwins);
     console.log("Your total losses: " + yourtotallosses);
